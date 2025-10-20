5c81f5f8e310d4fc9f7169eedc488b35c6aa32d0
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-10-20 15:29:18 -0300

    função para criar um NeoString (apenas um protótipo da ideia)

--- main.c
+++ main.c
@@ -6,19 +6,34 @@ typedef struct NeoString{
     char* string;
 }NeoString;
 
+NeoString createNeoString(int size) { //primeira ideia de como cirar um NeoString, ainda vou desenvolver mais isso
+    NeoString s;
+    s.size = size;
+    s.string = malloc(size * sizeof(char));
+    return s;
+}
+
 
 int main(){
 
     Clear();
-    printf("teste\n");
 
+    NeoString nome = createNeoString(3);
+    nome.string[0] = 's';
+    nome.string[1] = 'i';
+    nome.string[2] = 'm';
+
+for(int i = 0; i < nome.size; i++){
+    printf("%c", nome.string[i]);
+    printf("\n");
+}
+
+    /*
     NeoString teste;
-
     teste.size = 2;
     teste.string = 'a';
-
     printf("%c", teste.string);
-
+    */
 
 
     return 0;
