78f0d6435cb2aa75d273d2dad2c2efbbe4b69ebf
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-10-21 17:17:41 -0300

    Método para criar NeoString melhorado. Agora consigo criar a partir de expressões literais

--- main.c
+++ main.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 #include "portable_utils.h"
 
-typedef struct NeoString{
+typedef struct NeoString{ //talvez eu faça com que ele se tornem CONST posteriormente
     int size;
     char* string;
 }NeoString;
 
+
+NeoString createNeoString(const char* txt){
+    NeoString s;
+
+    int size = 0;
+    while (txt[size] != '\0'){
+        size++;
+    }
+
+    s.size = size;
+    s.string = malloc(size * sizeof(char));
+
+    for (int i = 0; i < size; i++) {
+        s.string[i] = txt[i];
+    }
+
+    return s;
+}
+
+
+/*
 NeoString createNeoString(int size) { //primeira ideia de como cirar um NeoString, ainda vou desenvolver mais isso
     NeoString s;
     s.size = size;
     s.string = malloc(size * sizeof(char));
     return s;
+}
+*/
+
+void printNeoString(NeoString s){
+
+    for(int i = 0; i < s.size; i++){
+        printf("%c", s.string[i]);
+    }
+
 }
 
 
@@ -18,15 +48,24 @@ int main(){
 
     Clear();
 
-    NeoString nome = createNeoString(3);
+    /*
+    NeoString nome = createNeoString(4);
     nome.string[0] = 's';
     nome.string[1] = 'i';
     nome.string[2] = 'm';
+    nome.string[3] = '\n';
+    */
 
-for(int i = 0; i < nome.size; i++){
-    printf("%c", nome.string[i]);
-    printf("\n");
-}
+    //printNeoString(nome);
+    //printf("\n");
+
+
+
+    NeoString teste = createNeoString("texto\n");
+
+    printNeoString(teste);
+
+    
 
     /*
     NeoString teste;
